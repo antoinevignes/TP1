79f393484a944a2defea8dadd3ed875094f37c09
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2024-12-16 14:07:45 +0100

    Afficher la liste et les couleurs en fonction de la priorité

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const tasks = [
+  {
+    title: "Apprendre mon cours de JavaScript",
+    priority: 1,
+  },
+  {
+    title: "Créer mon compte Github",
+    priority: 2,
+  },
+  {
+    title: "Répondre à mes emails",
+    priority: 3,
+  },
+];
+
+const list = document.querySelector("#list");
+
+function displayList(arr) {
+  let priorityClasses = ["high", "normal", "low"];
+
+  arr.forEach((item) => {
+    const listEl = document.createElement("li");
+
+    listEl.className = priorityClasses[item.priority - 1];
+
+    listEl.innerHTML = `<label>
+                          <input type="checkbox">
+                          ${item.title}
+                        </label>`;
+
+    list.appendChild(listEl);
+  });
+}
+
+displayList(tasks);
